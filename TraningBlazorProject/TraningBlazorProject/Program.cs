b931255c359a9c5e859b0a5718b4859af421520c
mahdi amini <classicus.ma@gmail.com>
2025-03-30 13:36:34 +0330

    Added some configs.

--- TraningBlazorProject/TraningBlazorProject/Program.cs
+++ TraningBlazorProject/TraningBlazorProject/Program.cs
@@ -1,4 +1,6 @@
+using TraningBlazorProject.Client.Common.Services_Extensions;
 using TraningBlazorProject.Client.Pages;
+using TraningBlazorProject.Client.Pages._4._40_CascadingParameter;
 using TraningBlazorProject.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+
+
+//4.40- CascadingParameter
+builder.Services.AddCascadingValue(p => new UserInfo() { Name = "Mahdi Amini" });
+builder.Services.AddCascadingValue("Username", p => new UserInfo() { UserName = "MahdiAmini_98" });
+
+//40.44 - virtualized
+//builder.Services.AddScoped<ProductService>();
+
+ServicesExtensions.AddCommonServices(builder.Services);
+
+
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -31,6 +47,6 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(TraningBlazorProject.Client._Imports).Assembly);
+    .AddAdditionalAssemblies(typeof(TraningBlazorProject.Client._Imports).Assembly).AddAdditionalAssemblies(typeof(TraningRCLProject._Imports).Assembly);
 
 app.Run();
