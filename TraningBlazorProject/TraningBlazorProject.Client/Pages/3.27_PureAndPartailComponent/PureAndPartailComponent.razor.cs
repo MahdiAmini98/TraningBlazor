21e51db50bbe6e427e490c081a588663a74b2d19
mahdi amini <classicus.ma@gmail.com>
2025-04-01 18:02:15 +0330

    Added Data Binding in Blazor.

---
+++ TraningBlazorProject/TraningBlazorProject.Client/Pages/3.27_PureAndPartailComponent/PureAndPartailComponent.razor.cs
@@ -0,0 +1,11 @@
+namespace TraningBlazorProject.Client.Pages._3_27_PureAndPartailComponent
+{
+    public partial class PureAndPartailComponent
+    {
+        public int number = 0;
+        public void AddNumber()
+        {
+            number++;
+        }
+    }
+}
---
+++ TraningBlazorProject/TraningBlazorProject.Client/Pages/3.27_PureAndPartailComponent/PureComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace TraningBlazorProject.Client.Pages._3._27_PureAndPartailComponent
+{
+    public class PureComponent : ComponentBase
+    {
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0,"div");
+            builder.OpenElement(1,"h6");
+            builder.AddContent(3,"mahdi jooon");
+            
+            builder.CloseElement();
+            builder.CloseElement();
+
+        }
+    }
+}
